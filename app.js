7e8f010b92dc491acc45eb973c52921888edf126
Gforneron <gonzaloforneron12@gmail.com>
2023-02-08 11:39:25 -0300

    Subida de mercado liebre

---
+++ app.js
@@ -0,0 +1,26 @@
+// LLamadas de Librerias
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 8080;
+
+// declaracion y uso de recursos de public
+
+const publico = path.resolve(__dirname, "./public");
+app.use(express.static(publico));
+
+// Levantar servidor
+
+app.listen(port, () => console.log("Corriendo servidor"));
+
+// Subir html
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
