eab2918ff8ab26eb56b92acca9226748c92f452f
Gforneron <gonzaloforneron12@gmail.com>
2023-02-08 13:49:36 -0300

    correccion de error en app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.port || 8080;
 
 // declaracion y uso de recursos de public
 
